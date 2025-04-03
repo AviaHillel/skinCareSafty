11653612852230955a0b19d0b731554df3695191
AviaHillel <avia.hillel@asperii.com>
2025-04-03 19:29:22 +0300

    basic structure

---
+++ skinCareSafty/src/main/java/com/skin/skinCareSafty/SkinCareSaftyApplication.java
@@ -0,0 +1,13 @@
+package com.skin.skinCareSafty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinCareSaftyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkinCareSaftyApplication.class, args);
+	}
+
+}
---
+++ skinCareSafty/src/test/java/com/skin/skinCareSafty/SkinCareSaftyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skin.skinCareSafty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinCareSaftyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
